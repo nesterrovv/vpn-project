4335cdedfbf1d6c26c86f17caac1ea02bfa71959
nesterrovv <nesterov.iv2018@gmail.com>
2023-10-26 21:27:49 +0300

    added dependencies to pom.xml and DB URL in application.properties

---
+++ src/main/java/com/nesterrovv/vpn/VpnApplication.java
@@ -0,0 +1,18 @@
+package com.nesterrovv.vpn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    private Application() {}
+
+    public static void main(String[] args) {
+        new SpringBootApplication().
+    }
+
+}
