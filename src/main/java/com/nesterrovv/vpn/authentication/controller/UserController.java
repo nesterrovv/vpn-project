037c4cde12224332e3af24f2598aa4f4ff8a94ea
Kirill <kirill.vorobyev2002@yandex.ru>
2023-10-26 19:37:58 +0300

    fix (controller): change @Controller to @RestController for send response to client

--- src/main/java/com/nesterrovv/vpn/authentication/controller/UserController.java
+++ src/main/java/com/nesterrovv/vpn/authentication/controller/UserController.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
