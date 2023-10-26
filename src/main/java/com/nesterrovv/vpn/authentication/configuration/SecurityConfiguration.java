69df782a5876ba95f7e9ddb85d7de6fb5ebda8bc
Kirill <kirill.vorobyev2002@yandex.ru>
2023-10-27 00:41:21 +0300

    feat (configuration): add filter by roles and password encoder

--- src/main/java/com/nesterrovv/vpn/authentication/configuration/SecurityConfiguration.java
+++ src/main/java/com/nesterrovv/vpn/authentication/configuration/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +16,17 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(config -> config.requestMatchers("/user/list").hasAuthority("ADMIN")
-                .anyRequest().permitAll());
+            .authorizeHttpRequests(config -> config
+                .requestMatchers("/user/list").hasAuthority("ADMIN")
+                .requestMatchers("/user/**").hasAuthority("USER")
+                .requestMatchers("/register", "/login").permitAll()
+                .anyRequest().authenticated());
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
