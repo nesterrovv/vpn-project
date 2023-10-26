ad52c4bb03670a6e6fc30b211d3fcdbecec6da7f
Kirill <kirill.vorobyev2002@yandex.ru>
2023-10-26 20:00:07 +0300

    feat (controller): add new url for testing security

--- src/main/java/com/nesterrovv/vpn/authentication/controller/UserController.java
+++ src/main/java/com/nesterrovv/vpn/authentication/controller/UserController.java
@@ -2,8 +2,9 @@ package com.nesterrovv.vpn.authentication.controller;
 
 import com.nesterrovv.vpn.authentication.entity.User;
 import com.nesterrovv.vpn.authentication.service.UserService;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,11 @@ public class UserController {
     @GetMapping("/{username}")
     public Optional<User> findUserByUsername(@PathVariable String username) {
         return userService.findByUsername(username);
+    }
+
+    @GetMapping("/list")
+    public List<User> getUsersList() {
+        return userService.listAll();
     }
 
 }
