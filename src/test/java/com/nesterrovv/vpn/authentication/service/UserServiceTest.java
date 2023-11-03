0e00b8773a6300172f37e7ecf54486cbb1b37611
Kirill <kirill.vorobyev2002@yandex.ru>
2023-11-03 16:41:32 +0300

    fix (test): rewrote existing tests for UserService

--- src/test/java/com/nesterrovv/vpn/authentication/service/UserServiceTest.java
+++ src/test/java/com/nesterrovv/vpn/authentication/service/UserServiceTest.java
@@ -1,34 +1,43 @@
 package com.nesterrovv.vpn.authentication.service;
 
 import com.nesterrovv.vpn.VpnApplication;
+import com.nesterrovv.vpn.authentication.dto.AuthenticationResponse;
 import com.nesterrovv.vpn.authentication.dto.RegisterDto;
+import com.nesterrovv.vpn.authentication.entity.Role;
 import com.nesterrovv.vpn.authentication.entity.User;
+import java.util.LinkedList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = VpnApplication.class)
 public class UserServiceTest {
 
-    @Autowired
+    @Mock
     private UserService userService;
 
     @Test
     void listAllTest() {
+        List<User> list = new LinkedList<>();
+        list.add(new User(1, "first", "ras", "first@mail.ru", Role.USER));
+        list.add(new User(2, "second", "dva", "second@mail.ru", Role.USER));
+        Mockito.when(userService.listAll()).thenReturn(list);
         List<User> result = userService.listAll();
-        for (User user : result) {
-            System.out.println(user);
-        }
         assertEquals(2, result.size());
+        assertEquals(1, result.getFirst().getId());
+        assertEquals("second", result.getLast().getUsername());
     }
 
     @Test
-    void createTest() {
-        int startValue = userService.listAll().size();
-        userService.register(new RegisterDto("test", "new", "test@mail.ru"));
-        assertEquals(startValue + 1, userService.listAll().size());
+    void registerTest() {
+        AuthenticationResponse response = new AuthenticationResponse("Ok", "a41h43");
+        RegisterDto dto = new RegisterDto("test", "new", "test@mail.ru");
+        Mockito.when(userService.register(dto)).thenReturn(response);
+        AuthenticationResponse result = userService.register(dto);
+        assertEquals(response, result);
     }
 
 }
