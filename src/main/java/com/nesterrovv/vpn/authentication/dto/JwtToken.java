0ad9124efdc1d4d8f9496c5ae638566d3181f455
Kirill <kirill.vorobyev2002@yandex.ru>
2023-11-08 23:49:55 +0300

    refactor: edited auth response dto

---
+++ src/main/java/com/nesterrovv/vpn/authentication/dto/JwtToken.java
@@ -0,0 +1,14 @@
+package com.nesterrovv.vpn.authentication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class JwtToken {
+
+    private String token;
+
+}
